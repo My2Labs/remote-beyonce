ed70f8d37eaec00b2d8b4bc459e32d8fa1de152f
Sharron <sking171@ford.com>
2022-04-17 09:57:34 -0500

    Work exercises.

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
-const express = require("express")
-const cors = require("cors")
+const songAPI = `https://beyonce-object-api.herokuapp.com/hits`
+const movieAPI = `https://beyonce-object-api.herokuapp.com/movies`
+const jokeAPI = `https://api.jokes.one/jod?category=animal`
 
-const { hits, movies } = require("./data")
+//Print all the songs
+function fetchHitSongs() {
+    const fetchData = fetch(songAPI)
+        .then(response => response.json())
+        .then(response => {
+            const hitSongs = response.hits.map(hit => hit.title)
+            const flexmain = document.querySelector(".flexmain")
+            const div = document.createElement("div")
+            const ul = document.createElement("ul")
+            const li = document.createElement("li")
+            ul.textContent = "Beyonce Hit Songs"
+            flexmain.append(div)
+            div.append(ul)
+            ul.append(li)
+            li.append(hitSongs)
+            return hitSongs
+        })
 
-const app = express()
-app.use(cors())
+    return fetchData
+}
 
-app.get("/hits", (request, response) => {
-  response.json({ hits })
-})
-app.get("/movies", (request, response) => {
-  response.json({ movies })
-})
+console.log(fetchHitSongs())
 
-app.listen(process.env.PORT || 8000)
+
+//Print all the movies
+function fetchHitMovies() {
+    const fetchDataMovies = fetch(movieAPI)
+        .then(response => response.json())
+        .then(response => {
+            const hitMovies = response.movies.map(hit => hit.title)
+            const flexmain = document.querySelector(".flexmain")
+            const div = document.createElement("div")
+            const ul = document.createElement("ul")
+            const li = document.createElement("li")
+            ul.textContent = "Beyonce Hit Movies"
+            flexmain.append(div)
+            div.append(ul)
+            ul.append(li)
+            li.append(hitMovies)
+            return hitMovies
+        })
+
+    return fetchDataMovies
+}
+
+console.log(fetchHitMovies())
+
+
+//Return an array of all Beyonce's hit song titles
