8e9511054177e206fa79f82892272c0e3ca4875f
My2Labs <sking171@ford.com>
2022-04-18 18:08:39 -0500

    Work exercises.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const songAPI = `https://beyonce-object-api.herokuapp.com/hits`
 const movieAPI = `https://beyonce-object-api.herokuapp.com/movies`
 
 
-//Print all the songs
+//1. Print all the songs
 function fetchHitSongs() {
     const fetchData = fetch(songAPI)
         .then(response => response.json())
@@ -12,23 +12,18 @@ function fetchHitSongs() {
             const div = document.createElement("div")
             const ul = document.createElement("ul")
             const li = document.createElement("li")
-            ul.textContent = "Beyonce Hit Songs"
+            ul.textContent = "1. Print all the songs"
             exercise.append(div)
             div.append(ul)
             ul.append(li)
             li.append(hitSongs)
             return hitSongs
         })
-
-   
 }
+fetchHitSongs()
 
-console.log(fetchHitSongs())
-
-
-
-//Print all the movies
-function fetchHitMovies() {
+//2. Print all the movies
+function fetchHitMovies(title) {
     const fetchDataMovies = fetch(movieAPI)
         .then(response => response.json())
         .then(response => {
@@ -37,14 +32,50 @@ function fetchHitMovies() {
             const div = document.createElement("div")
             const ul = document.createElement("ul")
             const li = document.createElement("li")
-            ul.textContent = "Beyonce Hit Movies"
+            ul.textContent = "2. Print all the movies"
             exercise.append(div)
             div.append(ul)
             ul.append(li)
             li.append(hitMovies)
             return hitMovies
         })
-
 }
+fetchHitMovies()
 
-console.log(fetchHitMovies())
+//3. Return an array of all Beyonce's hit song titles
+function fetchHitSongTitles() {
+    const fetchData = fetch(songAPI)
+        .then(response => response.json())
+        .then(response => {
+            const exercise = document.querySelector(".exercise")
+            const div = document.createElement("div")
+            const ul = document.createElement("ul")
+            const li = document.createElement("li")
+            ul.textContent = "3. Return an array of all Beyonce's hit song titles"
+            exercise.append(div)
+            div.append(ul)
+            ul.append(li)
+            const hitSongTitles = response.hits.forEach((hit) => {
+                li.append(` ${hit.title} `)
+           })
+
+
+        })
+
+    }
+
+            // const exercise = document.querySelector(".exercise")
+            // const div = document.createElement("div")
+            // const ul = document.createElement("ul")
+            // const li = document.createElement("li")
+            // ul.textContent = "Beyonce Hit Songs - Return an array of all Beyonce's hit song titles"
+            // exercise.append(div)
+            // div.append(ul)
+            // ul.append(li)
+            // li.append(hitSongTitles)
+            // return hitSongTitles
+            // console.log(hitSongTitles)
+        // })
+// }
+fetchHitSongTitles()
+// console.log(fetchHitSongTitles())
