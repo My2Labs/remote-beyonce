218dc07fecc784aea15052a75999348a77feaca4
Sharron <sking171@ford.com>
2022-04-18 05:23:45 -0600

    Update style.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const songAPI = `https://beyonce-object-api.herokuapp.com/hits`
 const movieAPI = `https://beyonce-object-api.herokuapp.com/movies`
-const jokeAPI = `https://api.jokes.one/jod?category=animal`
 
 
 //Print all the songs
@@ -9,19 +8,19 @@ function fetchHitSongs() {
         .then(response => response.json())
         .then(response => {
             const hitSongs = response.hits.map(hit => hit.title)
-            const flexmain = document.querySelector(".flexmain")
+            const exercise = document.querySelector(".exercise")
             const div = document.createElement("div")
             const ul = document.createElement("ul")
             const li = document.createElement("li")
             ul.textContent = "Beyonce Hit Songs"
-            flexmain.append(div)
+            exercise.append(div)
             div.append(ul)
             ul.append(li)
             li.append(hitSongs)
             return hitSongs
         })
 
-    return fetchData
+   
 }
 
 console.log(fetchHitSongs())
@@ -34,19 +33,18 @@ function fetchHitMovies() {
         .then(response => response.json())
         .then(response => {
             const hitMovies = response.movies.map(hit => hit.title)
-            const flexmain = document.querySelector(".flexmain")
+            const exercise = document.querySelector(".exercise")
             const div = document.createElement("div")
             const ul = document.createElement("ul")
             const li = document.createElement("li")
             ul.textContent = "Beyonce Hit Movies"
-            flexmain.append(div)
+            exercise.append(div)
             div.append(ul)
             ul.append(li)
             li.append(hitMovies)
             return hitMovies
         })
 
-    return fetchDataMovies
 }
 
 console.log(fetchHitMovies())
