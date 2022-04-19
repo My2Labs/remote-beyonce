74cf665f0c98f5cc31dfb757a9f8ee8d91a7ce06
Sharron <100609892+My2Labs@users.noreply.github.com>
2022-04-19 08:02:42 -0500

    Update js.

--- index.js
+++ index.js
@@ -3,8 +3,51 @@ const movieAPI = `https://beyonce-object-api.herokuapp.com/movies`
 
 
 //1. Print all the songs
+function fetchHitSongTitles() {
+    const fetchData1 = fetch(songAPI)
+        .then(response => response.json())
+        .then(response => {
+            const exercise = document.querySelector(".exercise")
+            const div = document.createElement("div")
+            const ul = document.createElement("ul")
+            const li = document.createElement("li")
+            ul.textContent = "1. Print all the songs"
+            exercise.append(div)
+            div.append(ul)
+            ul.append(li)
+            const hitSongTitles = response.hits.forEach((hit) => {
+                li.append(` ${hit.title} \n`)
+
+            })
+        })
+}
+fetchHitSongTitles()
+
+//2. Print all the movies
+function fetchHitMovies(title) {
+    const fetchData2 = fetch(movieAPI)
+        .then(response => response.json())
+        .then(response => {
+            const exercise = document.querySelector(".exercise")
+            const div = document.createElement("div")
+            const ul = document.createElement("ul")
+            const li = document.createElement("li")
+            ul.textContent = "2. Print all the movies"
+            exercise.append(div)
+            div.append(ul)
+            ul.append(li)
+            const hitMovies = response.movies.forEach((hit) => {
+                li.append(` ${hit.title} \n`)
+
+            })
+        })
+}
+fetchHitMovies()
+
+
+//3. Return an array of all Beyonce's hit song titles
 function fetchHitSongs() {
-    const fetchData = fetch(songAPI)
+    const fetchData3 = fetch(songAPI)
         .then(response => response.json())
         .then(response => {
             const hitSongs = response.hits.map(hit => hit.title)
@@ -12,7 +55,7 @@ function fetchHitSongs() {
             const div = document.createElement("div")
             const ul = document.createElement("ul")
             const li = document.createElement("li")
-            ul.textContent = "1. Print all the songs"
+            ul.textContent = "3. Return an array of all Beyonce's hit song titles"
             exercise.append(div)
             div.append(ul)
             ul.append(li)
@@ -22,60 +65,23 @@ function fetchHitSongs() {
 }
 fetchHitSongs()
 
-//2. Print all the movies
-function fetchHitMovies(title) {
-    const fetchDataMovies = fetch(movieAPI)
+
+//4. Return an array of all Beyonce's fierceness ratings
+function fetchFierceness() {
+    const fetchData4 = fetch(songAPI)
         .then(response => response.json())
         .then(response => {
-            const hitMovies = response.movies.map(hit => hit.title)
+            const hitSongs = response.hits.map(hit => hit.fierceness)
             const exercise = document.querySelector(".exercise")
             const div = document.createElement("div")
             const ul = document.createElement("ul")
             const li = document.createElement("li")
-            ul.textContent = "2. Print all the movies"
+            ul.textContent = "4. Return an array of all Beyonce's fierceness ratings"
             exercise.append(div)
             div.append(ul)
             ul.append(li)
-            li.append(hitMovies)
-            return hitMovies
+            li.append(hitSongs)
+            return hitSongs
         })
 }
-fetchHitMovies()
-
-//3. Return an array of all Beyonce's hit song titles
-function fetchHitSongTitles() {
-    const fetchData = fetch(songAPI)
-        .then(response => response.json())
-        .then(response => {
-            const exercise = document.querySelector(".exercise")
-            const div = document.createElement("div")
-            const ul = document.createElement("ul")
-            const li = document.createElement("li")
-            ul.textContent = "3. Return an array of all Beyonce's hit song titles"
-            exercise.append(div)
-            div.append(ul)
-            ul.append(li)
-            const hitSongTitles = response.hits.forEach((hit) => {
-                li.append(` ${hit.title} `)
-           })
-
-
-        })
-
-    }
-
-            // const exercise = document.querySelector(".exercise")
-            // const div = document.createElement("div")
-            // const ul = document.createElement("ul")
-            // const li = document.createElement("li")
-            // ul.textContent = "Beyonce Hit Songs - Return an array of all Beyonce's hit song titles"
-            // exercise.append(div)
-            // div.append(ul)
-            // ul.append(li)
-            // li.append(hitSongTitles)
-            // return hitSongTitles
-            // console.log(hitSongTitles)
-        // })
-// }
-fetchHitSongTitles()
-// console.log(fetchHitSongTitles())
+fetchFierceness()
